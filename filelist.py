25e8563319bba430bddebdd79be342c4b2845b07
KFmm <adelinea100@gmail.com>
2021-04-17 14:06:20 +0200

    filelist

--- filelist.py
+++ filelist.py
@@ -4,16 +4,16 @@ import os
 #si --checksum ajouter the file checksums
 
 #chaque fichier est transmis au fur et a mesure ? optimisation ?
-#trie de la liste ? par filelist ou ailleurs ? -> ailleurs
 
 def parcours_rec(dir):
     curr_dir = os.listdir(dir)
     file_list=[]
     for elt in curr_dir:
-        if os.path.isdir(os.path.join(dir,elt)):
-            file_list = file_list+[os.path.join(dir,elt)]+parcours_rec(os.path.join(dir,elt))
-        if os.path.isfile(os.path.join(dir,elt)):
-            file_list.append(os.path.join(dir,elt))
+        name = os.path.join(dir,elt)
+        if os.path.isdir(name):
+            file_list = file_list+[[name]+os.stat(name)]+parcours_rec(name))
+        if os.path.isfile(name):
+            file_list.append([name]+os.stat(name))
     return file_list
 
 def parcours(dir,dict):#atention affiche les fichiers cachés
@@ -25,14 +25,16 @@ def parcours(dir,dict):#atention affiche les fichiers cachés
     if dict['-r']:
         curr_dir = os.listdir(dir)
         for elt in curr_dir:
-            if os.path.isfile(os.path.join(dir,elt)):
-                file_list.append(os.path.join(dir,elt))
+            name = os.path.join(dir,elt)
+            if os.path.isfile(name):
+                file_list.append([name]+os.stat(name))
             else :
-                file_list=file_list+[os.path.join(dir,elt)]+parcours_rec(os.path.join(dir,elt))
+                file_list=file_list+[[name]+os.stat(name)]+parcours_rec(name))
     else :
         curr_dir = os.listdir(dir)
         for elt in curr_dir:
-            file_list.append(os.path.join(dir,elt))
+            name = os.path.join(dir,elt)
+            file_list.append([name]+os.stat(name))
     return file_list
 
 if __name__ == "__main__":
