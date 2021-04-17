9be347fe3a1d13afa535862f6f59199a30f5388c
KFmm <adelinea100@gmail.com>
2021-04-17 13:45:16 +0200

    cc

--- sender.py
+++ sender.py
@@ -3,10 +3,11 @@ import filelist
 #brouillon
 
 def send_listonly(lis_dir,dict):
+    file_list=[]
     for dir in lis_dir:
-        file_list = filelist.parcours(dir,dict)
-        for elt in file_list:
-            print(elt)
+        file_list = file_list + filelist.parcours(dir,dict)
+    for elt in file_list:
+        print(elt)
 
 def send_local(dir,dict):
     file_lists = filelist.parcours(dir,dict)
