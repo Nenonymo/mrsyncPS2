0ed1f824ce42bafccd2cfdfa421cb6a64fe4b474
KFmm <adelinea100@gmail.com>
2021-04-28 16:20:02 +0200

    filelist sender

--- filelist.py
+++ filelist.py
@@ -2,81 +2,86 @@ import os
 
 #si --checksum ajouter the file checksums
 
-def parcours_simple(dir):
+def parcours_simple(dir,nom_loc):
     curr_dir = os.listdir(dir)
     file_list=[]
     for elt in curr_dir:
         name = os.path.join(dir,elt)
+        if nom_loc.endswith('/'):
+            nom = nom_loc + elt
+        else :
+            nom = nom_loc +'/'+elt
         st = os.stat(name)
-        file_list.append({'name':name,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime})
+        file_list.append({'name_loc':nom,'name':name,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime})
     return file_list
 
-def parcours_rec(dir):
+def parcours_rec(dir,nom_loc):
     curr_dir = os.listdir(dir)
     file_list=[]
     for elt in curr_dir:
         name = os.path.join(dir,elt)
+        if nom_loc.endswith('/'):
+            nom = nom_loc+elt
+        else :
+            nom=nom_loc+'/'+elt
         st = os.stat(name)
         if os.path.isdir(name):
-            file_list = file_list+[{'name':name,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime}]+parcours_rec(name)
+            file_list = file_list+[{'name_loc':nom,'name':name,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime}]+parcours_rec(name,nom)
         else:
-            file_list.append({'name':name,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime})
+            file_list.append({'name_loc':nom,'name':name,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime})
     return file_list
 
-def parcours(dir,dic): #fichiers caches compris
+def parcours(dir,nom_loc,dic): #fichiers caches compris
     file_list=[]
     if dic['-r'] :
         if os.path.isdir(dir):
-            file_list = parcours_rec(dir)
+            file_list = parcours_rec(dir,nom_loc)
         else:
             st = os.stat(dir)
-            file_list.append({'name':dir,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime})
+            file_list.append({'name_loc':nom_loc,'name':dir,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime})
     else :
         if os.path.isdir(dir) and dir[-1]=='/':
-            file_list = parcours_simple(dir)
+            file_list = parcours_simple(dir,nom_loc)
         else :
             st=os.stat(dir)
-            file_list.append({'name':dir,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime})
+            file_list.append({'name_loc':nom_loc,'name':dir,'user':st.st_uid,'groupe':st.st_gid,'mode':st.st_mode,'size':st.st_size,'modtime':st.st_mtime})
     return file_list
 
 def norm_liste_dir(lis_dir, dic) :
-    #On créé la liste complète (sauf avec -r) de fichiers à traiter en faisant gaffe aux doublons
-    if dic['-r'] :
-        i = 0
-        while i < len(lis_dir) :
-            j = i + 1
-            while j < len(lis_dir) :
-                if lis_dir[i] == lis_dir[j][:len(lis_dir[i])] + '/' :
-                    del lis_dir[j]
-                elif lis_dir[i][:len(lis_dir[j])] + '/' == lis_dir[j] :
-                    del lis_dir[i]
-                j += 1
-            i += 1
-    else :
-        i = 0
-        while i < len(lis_dir) :
-            if lis_dir[i][-1]=='/':
-                lis_dir[i]= os.path.abspath(lis_dir[i])+'/'
-            else :
-                lis_dir[i] = os.path.abspath(lis_dir[i])
-            i += 1
-        i = 0
-        while i < len(lis_dir) :
-            j = i+1
-            while j < len(lis_dir) :
-                if lis_dir[i] == lis_dir[j] :
-                    del lis_dir[j]
-                j += 1
-            i += 1
-    return lis_dir
+    lis_dir_abs=[]
+    i = 0
+    while i < len(lis_dir) :
+        if lis_dir[i][-1]=='/':
+            lis_dir_abs.append(os.path.abspath(lis_dir[i])+'/')
+        else:
+            lis_dir_abs.append(os.path.abspath(lis_dir[i]))
+        if lis_dir[i].startswith('..'):
+            chemin_abs=os.getcwd().split("/")
+            chemin = lis_dir[i].split("/")
+            while chemin[0] == '..':
+                    chemin_abs.pop()
+                    chemin.pop(0)
+            lis_dir[i]="/".join([chemin_abs[-1]]+chemin)
+        elif lis_dir[i].startswith('./'):
+            lis_dir[i]=os.getcwd().split("/")[-1]+lis_dir[i][1:]
+        elif lis_dir[i]=='.':
+            lis_dir[i]=os.getcwd().split("/")[-1]
+        i += 1
+    i = 0
+    while i < len(lis_dir) :
+        j = i+1
+        while j < len(lis_dir) :
+            if lis_dir[i] == lis_dir[j] :
+                del lis_dir[j]
+            j += 1
+        i += 1
+    return lis_dir_abs,lis_dir
 
 def filelist(lis_dir,dic):
     file_list = []
-    lis_dir = norm_liste_dir(lis_dir,dic)
-    for dir in lis_dir:
-        file_list = file_list + parcours(dir,dic)
+    lis_dir_abs,lis_dir = norm_liste_dir(lis_dir,dic)
+    for i in range(len(lis_dir_abs)):
+        file_list = file_list + parcours(lis_dir_abs[i],lis_dir[i],dic)
     return file_list
 
-#il faut rajouter le nom local?
-#bug
-#affichage du nom absolu, j'ai du enlever le truc avec cwd provisoirement
+#on affiche le nom local, que faire quand plusieurs repertoires differents ? plus englobant ?
