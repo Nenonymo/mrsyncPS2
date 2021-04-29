faf1bf6833f90ae5077599e985fda9929a2a1ebd
gargaranza <newvalentin.43@gmail.com>
2021-04-29 12:31:30 +0200

    -h et -v update

--- options.py
+++ options.py
@@ -4,7 +4,14 @@ def parser(args) :
     args = args[1:]
     fichiers = []
     arguments = []
-    dic = {'-v':False, '-q':False, '-a':False, '-r':False, '-u':False, '-d':False, '-H':False, '-p':False, '-t':False, '--times':False, '--existing':False, '--ignore-existing':False, '--delete':False, '--force':False, '--timeout':0, '--blocking-io':False, '-I':False, '--ignore-times':False, '--size-only':False, '--adress':'', '--port':'', '--list-only':False, '-h':False, 'ssh':False, '--daemon':False, '--no-detach':False}
+    dic = {'-v':0, '-q':False, '-a':False, '-r':False, '-u':False, '-d':False, '-H':False, '-p':False, '-t':False, '--times':False, '--existing':False, '--ignore-existing':False, '--delete':False, '--force':False, '--timeout':0, '--blocking-io':False, '-I':False, '--ignore-times':False, '--size-only':False, '--adress':'', '--port':'', '--list-only':False, '-h':False, 'ssh':False, '--daemon':False, '--no-detach':False}
+    
+    if len(args) == 0 or (len(args) == 1 and args[0] == '-h') :
+        show_help('mrsync.txt')
+        sys.exit()
+    elif '--daemon' in args and '-h' in args :
+        show_help('mrsync_daemon.txt')
+    
     for i in args :
         if i[0] == '-' :
             arguments += [i]
@@ -15,6 +22,7 @@ def parser(args) :
                 dic['ssh'] = True
             fichiers += [i]
     if len(fichiers) == 0 :
+        show_help('mrsync.txt')
         raise Exception('Pas de fichier')
     elif len(fichiers) == 1 :
         destination = '.'
@@ -22,11 +30,16 @@ def parser(args) :
     else :
         destination = fichiers[-1]
         fichiers = fichiers[:-1]
-    
+
     for i in arguments :
         if '=' in i :
             j = i.split('=')
             dic[j[0]] = j[1]
+        elif i == '-v' :
+            if dic['-v'] >= 5 :
+                dic['-v'] = 5
+            else :
+                dic['-v'] += 1
         else :
             dic[i] = True
     
@@ -35,6 +48,11 @@ def parser(args) :
         for i in arguments :
             if i.split('=')[0] not in listDaemonArgs :
                 raise Exception('Arguments invalide')
-                
 
     return dic, fichiers, destination
+
+def show_help(fileName) :
+    file = open(fileName, 'r')
+    for i in file :
+        print(i, end='')
+    print()
