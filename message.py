ee3fc8b98804e7542aaaa06b9f6f7ee50c04be7a
Nenonymo <nchentre666@gmail.com>
2021-04-30 18:11:04 +0200

    fonctions envoit et recoit fonctionnelles, regardez la docstring pour l'utilisation de celles-ci

--- message.py
+++ message.py
@@ -5,16 +5,49 @@ des fonctions pour savoir si on affiche ou non qqchose (avec les options -v et -
 '''
 MAXBYTES = 100 #provisoire
 
+class ModeError(Exception): pass
+
 def recoit(fd):
-    v = os.read(fd,MAXBYTES)
-    #fonction de reception d'un message
+    '''reception et traitement d'un message
+    
+    Args:
+        fd (file descriptor): descriptor of the input file
+        
+    Returns:
+        tag (list): info about the file and the properties of the transmission (localName, cat, transmissionNumber)
+        msg (string): content of the file'''
+
+    dataRaw = str(os.read(fd, 100))[2:-1] #read the whole message
+
+    #Tag
+    tagRaw = dataRaw.split('\\n')[0] #Première ligne uniquement
+    tagRawL = tagRaw.split(' ')
+    tag = [tagRawL[0], tagRawL[1]] #localFileName and transmission category
+    tag.append(tuple(map(int, tagRawL[2].split('_')))) #transmission num
+    
+    #Msg
+    msg = dataRaw[len(tagRaw)+2:] #Everything but the first line
+    
     return tag,msg
 
-def envoit(fd,tag,v):
-    #a preciser, le tag ne sera pas tjr le nom
-    if os.path.isfile(tag):
-        file = open(tag)
-        #envoit le contenu du fichier
-    elif tag == "liste" or tag=="listef":
-        print()
-        #envoit le fichier v
+def envoit(fd,tag,v=''):
+    '''Wrtiting the message on the pipe
+
+    Args:
+        fd (file descriptor): descriptor of the output pipe, must be opened with `fd = os.fdopen(fd, 'w')`
+        tag (struct): info about the file and the properties of the transmission (localName, cat, transmissionNumber)
+        v (string): content to write
+    
+    Returns:
+        None
+    '''
+
+    if tag[1] == 'r' or tag[1] == 'l':
+        content = "{} {} {}_{}\n{}".format(tag[0], tag[1], tag[2][0], tag[2][1], tag[0])
+    elif tag[1] == 'f':
+        content = "{} {} {}_{}\n{}".format(tag[0], tag[1], tag[2][0], tag[2][1], v)
+    else:
+        raise ModeError("The send mode isn't of the followings: [f=file, r=dir, l=list]")
+
+    fd.write(content)
+    return(0)
