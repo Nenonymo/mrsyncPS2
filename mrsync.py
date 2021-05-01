cc7916ac13aae2cf22426aaab5f4366dcb505cb3
Nenonymo <nchentre666@gmail.com>
2021-05-01 18:48:57 +0200

    now storing the pid

--- mrsync.py
+++ mrsync.py
@@ -1,7 +1,10 @@
-import os, sys, options, sender, server
+import os, sys, options, sender, server, pidManagement
 
 if __name__ == '__main__' :
     dic, src, dest = options.parser(sys.argv)
+
+    if os.path.exists("pid"): #a placer à la fin de l'éxécution du programme, quand tout est fini
+        os.remove("pid")
     
     if dic['--list-only'] :    #gerer le liste only avec le ssh dans list only ou ssh ?
         sender.send_listonly(src, dic)
@@ -13,7 +16,9 @@ if __name__ == '__main__' :
         sr_r,sr_s = os.pipe()
         gs_s,gs_g = os.pipe()
         pid=os.fork()
-        if pid == 0 :
+        if pid != 0 : #pere, server
+            pidManagement.appToFile('pid', 'ge:{}'.format(os.getpid()))
             server.server_local(src,dest,dic,gs_g,sr_r)
-        else :
+        else : #fils, sender
+            pidManagement.appToFile('pid', 'se:{}'.format(os.getpid())) #enregistrement du pid
             sender.send_local(src,dic,gs_s,sr_s)
--- receiver.py
+++ receiver.py
@@ -1,4 +1,4 @@
-import os,filelist,message,generator
+import os,filelist,message,generator, pidManagement
 
 #brouillon
 
@@ -13,14 +13,16 @@ def receive_local(dirs,dirr,dic,gs_g,sr_r):
         print(data)
         file_lists.append(data)
     pid=os.fork()
-    if pid == 0:
+    if pid != 0: #père, générateur
         generator.generator_local(dirs,dirr,file_lists,file_listr,dic,gs_g)
-    else:
+    else: #fils, receiver
+        pidManagement.appToFile('pid', 're:{}'.format(os.getpid()))#enregistrement du pid
         tag,data = message.recoit(sr_r)
         nbr_file = tag[2][1]
-        i=1
         print(nbr_file)
-        '''while i <= nbr_file:
+        '''
+        i = 1
+        while i <= nbr_file:
             tag,data = message.recoit(sr_r)
             #ecrit les donnes dans le fichier + cree les repertoires
             i+=1
