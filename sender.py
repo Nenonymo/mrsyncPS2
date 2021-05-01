ff2bafac513257a065f7130a492cb16d6f28e1fd
KFmm <adelinea100@gmail.com>
2021-05-01 13:12:18 +0200

    sender message

--- sender.py
+++ sender.py
@@ -15,31 +15,31 @@ def send_local(dir,dic,gs_s,sr_s): #s'occupe des checksum
     sr = os.open(sr_s,os.O_WRONLY)
     gs = os.open(gs_s,os.O_RDONLY)
     nbr_file = len(file_list)
-    for i in range(nbr_file):
+    for i in range(nbr_file):     #envoit de la liste au receveur
         tag = [file_list[i]['name_loc'],'l',(i,nbr_file)]
-        message.envoit(sr,tag,file_list[i]) #envoit la liste de fichier au receveur
+        message.envoit(sr,tag,file_list[i])
     tag,data = message.recoit(gs)
     nbr_file=tag[2][1]
     tag_e = ['','l',(0,nbr_file)]
     message.envoit(sr,tag_e)   #on envoit le nbr de fichiers a traiter
     while tag[2][0] <= nbr_file : #si send_list est vide, on rentre pas dans la boucle
-        #traitement du premier message ici
-        tag,data = message.recoit(gs)
-        tag_e = [tag[0]]
-        filename = os.path.abspath(tag2)
-        tag2 += "d"
-        #tester si un seul envoit necessaire mettre df
-        while tag2 != "" :
-            #data2 = os.read(filename,n)
-            message.envoit(sr_s,tag2,data2)
-            if tag2 == data+"d":
-                tag2 = data
-            '''if #dernier :
-                tag2 += "f"
-            elif tag2 == data+"f" or tag2 == data+"df":
-                tag2 = ""'''
+        if os.path.isdir(data['name']):  #repertoire
+            tag_e = [tag[0],'r',(1,1)]
+            message.envoit(sr,tag_e,data)
+        elif os.path.islink(data['name']): #lien symbolique
+            tag_e = [tag[0],'s',(1,1)]
+            message.envoit(sr,tag_e,data)
+        elif os.path.isfile(data['name']): #fichier
+            fd = os.open(data['name'],os.O_RDONLY)
+            size_file = data['size']
+            #envoyer le contenu du fichier
+
+        tag,data = message.recoit(gs) #prochain fichier a traiter
 
 
-        #envoit les fichiers correspondant a receiver
-        #ouvre le fichier, le lit et l'envoie au receveur en plusieurs messages(+ gros que 16 Mo)
-        #tag = quel fichier il s'agit+tag data pour les messages contennant des bytearray+tagfin d'envoie
+#autres types de fichiers ???
+        
+#a faire : checksum
+
+#ouvre le fichier, le lit et l'envoie au receveur en plusieurs messages(+ gros que 16 Mo)
+#tag = quel fichier il s'agit+tag data pour les messages contennant des bytearray+tagfin d'envoie
