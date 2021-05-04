a2949f7c931a1b5cc599dd595c47b40b7ed02653
gargaranza <newvalentin.43@gmail.com>
2021-05-05 01:23:49 +0200

    ajout de --server

--- options.py
+++ options.py
@@ -4,7 +4,7 @@ def parser(args) :
     args = args[1:]
     fichiers = []
     arguments = []
-    dic = {'-v':0, '-q':False, '-a':False, '-r':False, '-u':False, '-d':False, '-H':False, '-p':False, '-t':False, '--existing':False, '--ignore-existing':False, '--delete':False, '--force':False, '--timeout':0, '--blocking-io':False, '-I':False, '--size-only':False, '--adress':'', '--port':'', '--list-only':False, '-h':False, 'ssh':False, '--daemon':False, '--no-detach':False}
+    dic = {'-v':0, '-q':False, '-a':False, '-r':False, '-u':False, '-d':False, '-H':False, '-p':False, '-t':False, '--existing':False, '--ignore-existing':False, '--delete':False, '--force':False, '--timeout':0, '--blocking-io':False, '-I':False, '--size-only':False, '--adress':'', '--port':'', '--list-only':False, '-h':False, 'ssh':False, '--daemon':False, '--no-detach':False, 'daemonserveur':False, '--server':False}
     if len(args) == 0 or '-h' in args or '--help' in args :
         show_help('mrsync.txt')
         sys.exit(0)
@@ -14,14 +14,14 @@ def parser(args) :
         else :
             if '::' in i :
                 dic['--daemon'] = True
-            elif ':' in i and not('::' in i):
+            elif ':' in i :
                 dic['ssh'] = True
             fichiers += [i]
-    if len(fichiers) == 0 and not '--daemon' in args :
+    if len(fichiers) == 0 and '--daemon' in args : 
+        dic['daemonserveur'] = True
+    elif len(fichiers) == 0 :
         show_help('mrsync.txt')
         raise Exception('Pas de fichier')
-    elif len(fichiers) == 0 and '--daemon' in args : 
-        dic['daemonserveur']= True
     elif len(fichiers) == 1 :
         destination = '.'
         dic['--list-only'] = True
@@ -50,7 +50,7 @@ def parser(args) :
             dic[i] = True
     
     if dic['--daemon'] :
-        listDaemonArgs = ['--daemon', '--address', '--no-detach', '--port', '-h']
+        listDaemonArgs = ['--daemon', 'daemonserveur', '--address', '--no-detach', '--port', '-h']
         for i in arguments :
             if i.split('=')[0] not in listDaemonArgs :
                 raise Exception('Arguments invalide')
