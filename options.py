e41e5d59dceea358f4904fa351ba4c11cea50cdb
KFmm <adelinea100@gmail.com>
2021-05-08 12:18:54 +0200

    options

--- options.py
+++ options.py
@@ -10,11 +10,21 @@ def parser(args) :
     args = args[1:]
     fichiers = []
     destination = ''
-
+    dic = {'-v':0, '--timeout':0,'-q':False, '-a':False, '-r':False, '-u':False, '-d':False,
+        '-H':False, '-p':False, '-t':False, '--existing':False, '--ignore-existing':False,
+        '--delete':False, '--force':False, '--blocking-io':False, '-I':False, '--size-only':False, 
+        '--list-only':False, '-h':False, 'ssh':False, 'daemon':False, '--no-detach':False,
+        '--daemon':False, '--server':False,'push':False,'pull':False,'--address':'', '--port':''}
+    if len(args) == 0 :
+        show_help()
+        sys.exit(0)
     for i in range(len(args)) :
+        if pas:
+            pas=False
+            pass
         if args[i][0] == '-' :
-            try : dic = argument_management(args[i],args[i+1])
-            except : dic = argument_management(args[i],args[i])
+            try : dic,pas = argument_management(dic,args[i],args[i+1])
+            except : dic,pas = argument_management(dic,args[i],args[i])
         else :
             if '::' in args[i] :
                 dic['daemon'] = True
@@ -55,8 +65,11 @@ def argument_management(dic, elt, elt_next) :
             elt = ième élément de argv (string)
             elt_next = (i+1)ème élément de argv (string)
     output : dic = dictionnaire des options (dictionnaire)
+             pas = dis si on passe l'argument suivant ou non (booleen)
     '''
-    convert = {'--verbose':'-v','--quiet':'-q','--archive':'-a','--recursive':'-r','--update':'-u','--dirs':'-d','--hard-links':'-H','--perms':'-p','--times':'-t','--ignore-times':'-I','--help':'-h'}
+    pas = False
+    convert = {'--verbose':'-v','--quiet':'-q','--archive':'-a','--recursive':'-r','--update':'-u','--dirs':'-d',
+                '--hard-links':'-H','--perms':'-p','--times':'-t','--ignore-times':'-I','--help':'-h'}
     try : elt = convert[elt]
     except : None
     
@@ -65,15 +78,21 @@ def argument_management(dic, elt, elt_next) :
         sys.exit(0)
     elif '=' in elt :   #s'il y a '=' on fait l'affectation
         j = elt.split('=')
-        dic[j[0]] = j[1]
+        if j[0] == '--timeout' or j[0] == '--port':
+            dic[j[0]] = int(j[1])
     elif elt in ['--port','--timeout','--address'] : #on prends en compte si le séparateur n'est pas '=' mais ' '
-        dic[elt] = elt_next
+        if elt == '--timeout' or let == '--port':
+            dic[elt] = int(elt_next)
+        else :
+            dic[elt] = elt_next
+        pas = True
     elif elt == '-q' :      #mode quiet, on enlève la verbose
         dic['-q'] = True
         dic['-v'] = 0
-    elif elt == '-v' and not dic['-q'] and dic['-v'] != 3 : #si on veut ajouter de la verbose et qu'on n'est pas en mode -quiet(il est prioritaire) et on limite la verbose à 3
-        dic['-v'] += 1
+    elif '-v' in elt : #si on veut ajouter de la verbose et qu'on n'est pas en mode -quiet(il est prioritaire) et on limite la verbose à 3
+            if not dic['-q'] : 
+                dic['-v'] = min(elt.count('v'),3)
     else :                   #cas par défaut on set l'argument à True
         dic[elt] = True
     
-    return dic
+    return dic,pas
